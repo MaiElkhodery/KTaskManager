13442c7e8e7c865ea22d24f535d7aec9957881e3
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-01-30 14:12:40 +0200

    update token expiration date

--- src/main/kotlin/com/Kmongo/utils/JWTConfig.kt
+++ src/main/kotlin/com/Kmongo/utils/JWTConfig.kt
@@ -14,13 +14,14 @@ object JWTConfig {
     val ISSUER = config.property("jwt.issuer").getString()
     val AUDIENCE = config.property("jwt.audience").getString()
     val REALM = config.property("jwt.realm").getString()
+    val EXPIRE_TIME = 86400000
 
     fun getToken(id: String): String {
         return JWT.create()
             .withAudience(AUDIENCE)
             .withIssuer(ISSUER)
             .withClaim("id", id)
-            .withExpiresAt(Date(System.currentTimeMillis() + 600000))
+            .withExpiresAt(Date(System.currentTimeMillis() + EXPIRE_TIME))
             .sign(Algorithm.HMAC256(SECRET))
     }
 }
